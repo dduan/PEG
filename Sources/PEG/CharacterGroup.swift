bd12101634eb953e00ac836d7154d53eb291b63e
Daniel Duan <daniel@duan.ca>
2017-10-29 23:27:05 -0700

    Stub In CharacterGroup Interface

--- Sources/PEG/CharacterGroup.swift
+++ Sources/PEG/CharacterGroup.swift
@@ -1,1 +1,26 @@
-public struct CharacterGroup {}
+// [a-d] + [c-g] = [a-g]
+// [a-d] + [g-g] = [a-d, g-g]
+// [a-c] + [d-d] = [a-d]
+
+extension Character {
+    func isNextTo(_ other: Character) -> Bool {
+        fatalError()
+    }
+}
+
+extension ClosedRange where Bound == Character {
+    func inMergable(_ other: ClosedRange<Character>) -> Bool {
+        fatalError()
+    }
+}
+
+public struct CharacterGroup {
+    var ranges = [ClosedRange<Character>]()
+
+    func insert(_ range: ClosedRange<Character>) {
+    }
+
+    func contains(_ character: Character) -> Bool {
+        fatalError()
+    }
+}
