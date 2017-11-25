3a43b63ec8310fcae22f8eeb1edad39817a46c4f
Daniel Duan <daniel@duan.ca>
2017-11-24 23:42:08 -0800

    Add DotRepresentable

---
+++ Sources/PEG/DotRepresentable.swift
@@ -0,0 +1,28 @@
+protocol DotRepresentable {
+    var dotTitle: String { get }
+    var dotChildren: [Self] { get }
+}
+
+extension DotRepresentable {
+    public var dotRepresentation: String {
+        var paths = [(Self, Self)]()
+        self.visit(self, &paths)
+        let formatted = paths
+            .map { (path: (Self, Self)) -> String in
+                return "    \"\(path.0.dotTitle) -> \(path.1.dotTitle)\";"
+            }
+            .joined(separator: "\n")
+            return """
+            digraph graphname {
+            \(formatted)
+            }
+            """
+    }
+
+    private func visit(_ node: Self, _ paths: inout [(Self, Self)]) {
+        for child in node.dotChildren {
+            paths.append((node, child))
+            visit(child, &paths)
+        }
+    }
+}
