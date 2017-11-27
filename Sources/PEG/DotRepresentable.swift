cfbbbf70bf63fcf20be25ee1943ec304d9062db3
Daniel Duan <daniel@duan.ca>
2017-11-27 09:47:12 -0800

    Add DotRepresentable

--- Sources/PEG/DotRepresentable.swift
+++ Sources/PEG/DotRepresentable.swift
@@ -4,12 +4,14 @@ protocol DotRepresentable {
 }
 
 extension DotRepresentable {
+    typealias Node = (Self, Int)
     public var dotRepresentation: String {
-        var paths = [(Self, Self)]()
-        self.visit(self, &paths)
+        var paths = [(Node, Node)]()
+        var id = 0
+        self.visit(self, &paths, &id)
         let formatted = paths
-            .map { (path: (Self, Self)) -> String in
-                return "    \"\(path.0.dotTitle)\" -> \"\(path.1.dotTitle)\";"
+            .map { (path: (Node, Node)) -> String in
+                return "    \"\(path.0.1)\n\n\(path.0.0.dotTitle)\" -> \"\(path.1.1)\n\n\(path.1.0.dotTitle)\";"
             }
             .joined(separator: "\n")
             return """
@@ -19,10 +21,12 @@ extension DotRepresentable {
             """
     }
 
-    private func visit(_ node: Self, _ paths: inout [(Self, Self)]) {
+    private func visit(_ node: Self, _ paths: inout [(Node, Node)], _ id: inout Int) {
+        let parentID = id
         for child in node.dotChildren {
-            paths.append((node, child))
-            visit(child, &paths)
+            id += 1
+            paths.append(((node, parentID), (child, id)))
+            visit(child, &paths, &id)
         }
     }
 }
