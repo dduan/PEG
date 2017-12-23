138f630a0a57e78c8db77517716bfea997808fb3
Daniel Duan <daniel@duan.ca>
2017-12-22 22:07:37 -0800

    Delete Empty Line

--- Sources/playground/main.swift
+++ Sources/playground/main.swift
@@ -4,7 +4,6 @@ let input = """
     Arithmetic <- Factor AddExpr*
     AddExpr    <- ('+' / '-') Factor
     Factor     <- Primary MulExpr*
-
     MulExpr    <- ('*' / '/') Primary
     Primary    <- '(' Arithmetic ')' / Number
     Number     <- [0-9]+
