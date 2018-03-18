7bf1d3f90f81d08b8795a9c5fe3fb32efbc3990a
Daniel Duan <daniel@duan.ca>
2018-03-17 23:56:48 -0700

    add left recursion tests

---
+++ Tests/PEGTests/LeftRecursionTests.swift
@@ -0,0 +1,83 @@
+@testable import PEG
+import XCTest
+
+final class LeftRecursionTests: XCTestCase {
+    func assureInfiniteRecursion(_ name: String, _ definition: String, file: StaticString = #file, line: UInt = #line)
+    {
+        XCTAssertThrowsError(try Grammar(rootName: name, definition), file: file, line: line) { error in
+            guard case GrammarValidationError.infiniteRecursion = error else {
+                XCTFail()
+                return
+            }
+        }
+    }
+
+    func assureNoInfiniteRecursion(_ name: String, _ definition: String, file: StaticString = #file, line: UInt = #line)
+    {
+        XCTAssertNoThrow(try Grammar(rootName: "start", definition))
+    }
+
+    func testDirectLeftRecursion() {
+        assureInfiniteRecursion("start", "start <- start")
+    }
+
+    func testIndirectLeftRecursion() {
+        let test =
+            """
+            start <- stop
+            stop <- start
+            """
+
+        assureInfiniteRecursion("start", test)
+    }
+
+    func testLeftRecursionInEmptySequence() {
+        assureInfiniteRecursion("start", "start <- '' '' '' start")
+    }
+
+    func testLeftRecursionInNonEmptySequence() {
+        let tests = [
+            "start <- 'a' '' '' start",
+            "start <- '' 'a' '' start",
+            "start <- '' '' 'a' start",
+        ]
+
+        for test in tests {
+            assureNoInfiniteRecursion("start", test)
+        }
+    }
+
+    func testLeftRecursionInSequenceAndWrappedInOptional() {
+        assureInfiniteRecursion("start", "start <- '' start?")
+    }
+
+    func testLeftRecursionInSequenceWithOneOf() {
+        assureInfiniteRecursion("start", "start <- ('' / 'a' / 'b') start")
+        assureInfiniteRecursion("start", "start <- ('a' / '' / 'b') start")
+        assureInfiniteRecursion("start", "start <- ('a' / 'b' / '') start")
+        assureNoInfiniteRecursion("start", "start <- ('a' / 'b' / 'c') start")
+    }
+
+    func testLeftRecursionInSequenceWithPredicate() {
+        assureInfiniteRecursion("start", "start <- &'' start")
+        assureInfiniteRecursion("start", "start <- &'a' start")
+
+        assureInfiniteRecursion("start", "start <- !'' start")
+        assureInfiniteRecursion("start", "start <- !'a' start")
+    }
+
+    func testLeftRecursionInSequenceWithOptional() {
+        assureInfiniteRecursion("start", "start <- ''? start")
+        assureInfiniteRecursion("start", "start <- 'a'? start")
+    }
+
+    func testLeftRecursionInSequenceWithZeroOrMore() {
+        assureInfiniteRecursion("start", "start <- ''* start")
+        assureInfiniteRecursion("start", "start <- 'a'* start")
+    }
+
+    func testLeftRecursionInSequenceWithOneOrMore() {
+        assureInfiniteRecursion("start", "start <- ''+ start")
+        assureNoInfiniteRecursion("start", "start <- 'a'+ start")
+    }
+}
